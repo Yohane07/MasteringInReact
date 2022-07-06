b895b239f1acb04b6c2bf442e9df6d28bf91d910
pageAccueil <yohaneendoke4@gmail.com>
2022-07-07 01:37:01 +0200

    Rect 4 - Composant - 12 minutes restant

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ window.setInterval(()=>{
 
 // Les propos dans les fonctions React ont droit à des enfants: children
 
-function testComposant({name, children}){
+function WelcomeFunc({name, children}){
     return  <div>
             <h1> Hey {name}</h1>  
              <p>
@@ -81,10 +81,117 @@ function testComposant({name, children}){
     </div> 
 }
 
+// ReactDOM.render(<Welcome name ="Yohane">Aller t'as capté t'es un bon !</Welcome>,document.querySelector('#appjs'));
+
 /*Okay donc Welcome est une fonction React tu peux pas changer son nom et utliser ses propirétés le navigateur
 ne reconnaitra pas comme ici reconnaissait pas ma fonction quand je l'appelait testCOMPOSANT 
 
 Et pour que le name et le children soit pris en comptes tous deux il faut impérativement des "{}" autour des deux
 */
 
-ReactDOM.render(< testComposant name ="Yohane">Aller t'as capté t'es un bon !</testComposant>,document.querySelector('#appjs'));
+/* Y a une autre façon d'écrire les composants React c'est en créant des qui sont extends 
+de React.component et donc possède ses propriétés et fonctionnent comme les composants React
+*/
+
+class Welcome extends React.Component{
+
+    render() {
+        return <div>
+           <h1>Bonjour {this.props.name}</h1>
+        <p>
+            {this.props.children}
+        </p>
+        </div>
+    }
+}
+
+class Horloge extends React.Component{
+
+    constructor (props){
+        super(props)
+        this.state = {date: new Date()}
+        this.timer = null;
+    }
+
+    componentDidMount (){
+        this.timer = window.setInterval(this.tick.bind(this), 1000)
+
+    }
+
+    componentWillUnmount(){
+        window.clearInterval(this.timer)
+    }
+
+    tick () {
+        this.setState ({date: new Date()})
+    }
+
+
+
+    render() {
+        const date = new Date();
+
+        return <div>
+            <p>
+                Nous sommes le {date.toLocaleDateString()} et il est exactement {date.toLocaleTimeString()}
+            </p>
+        </div>
+    }
+   
+    
+}
+
+
+// Une composant qui incrémente un chiffre toute les seconcdes
+
+
+class Incremente extends React.Component{
+
+    constructor (props){
+        super (props)
+        this.state = {nombre: new nombre() }
+        this.timer = null;
+    }
+
+    componentDidMount (){
+        this.timer = window.setInterval(this.tick.bind(this), 1000)
+
+    }
+
+    componentWillUnmount(){
+        window.clearInterval(this.timer)
+    }
+
+
+    tick () {
+        this.setState ({nombre : new nombre() })
+    }
+    
+    render (){
+        const nombre = 0;
+        return <div>
+            <strong> Mon incrermentatioin {nombre}</strong>
+        </div>
+    }
+}
+
+/* 
+    mais ça ne nous permet d'avoir quelque chose de dynamique l'heure ne bouge pour avoir quelque de dynamique qui bouge 
+    sans avoir à réactualiser notre page on va utiliser ce qu'on appelle un état pour ça on aura besoin des constructeurs
+*/
+
+function Home (){
+    return <div>
+        <Welcome name = "Yohane" />
+        <Welcome name = "@m-yohane"/>
+        <Horloge />
+        <Incremente />
+    </div>
+}
+
+ReactDOM.render(<Home/>, document.querySelector('#appjs'))
+
+
+
+
+
